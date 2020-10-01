5e0356487bd8435fffdb14cbf08449b88c7827a3
Sophie <hellosophieee@hotmail.com>
2020-10-01 22:24:46 +0500

    feat: fix arr loop

--- loops-for/arrayLoop.py
+++ loops-for/arrayLoop.py
@@ -1,5 +1,11 @@
+arr = [1, 2, 3]
+
+
 def insert_squares(arr, num):
     # add square of numbers from 1 to num to the list named arr and return list
-    for i in range(1, num):
-        arr.append(i * i)
+    for i in range(1, num + 1):
+        arr.append(i ** 2)
     return arr
+
+
+print(insert_squares(arr, 5))
